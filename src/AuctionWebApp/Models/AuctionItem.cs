2a13ce4b8b5a944e73b5394daf50c13e8454c048
Douglas Purdy <dpurdy@my.wgu.edu>
2017-08-29 07:01:49 -0400

    model update

---
+++ src/AuctionWebApp/Models/AuctionItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuctionWebApp.Models
+{
+    public class AuctionItem
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public int minimumBid { get; set; }
+        public int numberOfBids { get; set; }
+
+    }
+}
