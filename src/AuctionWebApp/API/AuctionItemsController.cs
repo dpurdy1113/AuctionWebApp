9f2c31388f8e6e6db97e7c1d5acf1d446209d118
Douglas Purdy <dpurdy@my.wgu.edu>
2017-08-31 05:57:44 -0400

    added controllers

---
+++ src/AuctionWebApp/API/AuctionItemsController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AuctionWebApp.Models;
+using AuctionWebApp.Data;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AuctionWebApp.API
+{
+    [Route("api/[controller]")]
+    public class AuctionItemsController : Controller
+    {
+        ApplicationDbContext _db;
+        
+        public AuctionItemsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        // GET: api/values
+        [HttpGet]
+        public ICollection<AuctionItem> Get()
+        {
+            return _db.AuctionItems.ToList();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
